09d006d0621372b5a1ef255e380ce31158dcb052
vaishnavi <nayakvaishnavi88@gmail.com>
2025-04-06 09:56:52 +0530

    Ready for build

--- main.js
+++ main.js
@@ -1,41 +1,38 @@
-const { app, BrowserWindow } = require("electron");
-const express = require("express");
-const cors = require("cors");
-const { exec } = require("child_process");
-const si = require("systeminformation");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const si = require('systeminformation');
 
-const server = express();
-server.use(cors());
-
-server.get("/stats", async (req, res) => {
-  try {
-    const cpuUsage = await si.currentLoad();
-    const memory = await si.mem();
-    const disk = await si.fsSize();
-    const network = await si.networkStats();
-
-    res.json({
-      cpu_usage: cpuUsage.currentLoad.toFixed(2),
-      memory_usage: ((memory.used / memory.total) * 100).toFixed(2),
-      disk_usage: ((disk[0].used / disk[0].size) * 100).toFixed(2),
-      upload_speed: (network[0].tx_sec / 1024 / 1024).toFixed(2), // MB/s
-      download_speed: (network[0].rx_sec / 1024 / 1024).toFixed(2), // MB/s
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),  // Optional or remove
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
     });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch system stats" });
-  }
-});
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+    win.loadFile('index.html');
 
-app.whenReady().then(() => {
-  const win = new BrowserWindow({
-    width: 900,
-    height: 700,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    setInterval(async () => {
+        const cpu = await si.currentLoad();
+        const memory = await si.mem();
+        const disk = await si.fsSize();
+        const network = await si.networkStats();
 
-  win.loadFile("index.html");
+        win.webContents.send('system-stats', {
+            cpu_usage: cpu.currentLoad.toFixed(2),
+            memory_usage: ((memory.used / memory.total) * 100).toFixed(2),
+            disk_usage: disk[0].use.toFixed(2),
+            upload_speed: (network[0].tx_sec /1024/ 1024).toFixed(2),
+            download_speed: (network[0].rx_sec /1024/ 1024).toFixed(2),
+        });
+    }, 1000);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
 });
